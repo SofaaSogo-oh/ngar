ffdd82240371e2d2d807d94b1bef9e4681b16fa2
SofaaSogo(oh) <NaumikhinDD@stud.kai.ru>
2025-06-27 21:54:27 +0300

    Алгоритм завершен

--- main.lisp
+++ main.lisp
@@ -26,10 +26,8 @@
             (find-hull mask convex)))
     res-table))
 
-(defun check-ngar-display (convex ngar applier &key (os t))
-  (let* ((masks (get-all-masks-for-convex convex))
-         (check-masks (find-check-masks masks ngar))
-         (g-table (hulls-to-hashmap convex check-masks)))
+(defun check-ngar-display (masks convex check-masks applier &key (os t))
+  (let* ((g-table (hulls-to-hashmap convex check-masks)))
     (format os "~&G = ~a ~%"
             (print-set (mapcar (curry #'print-mask applier) convex)))
     (loop for mask in masks do
@@ -46,26 +44,75 @@
                            (format os "~a ⊄ ~a~%" d-key d-mask))))
                      g-table)))))
 
+
+(defun check-ngar (masks convex check-masks)
+  (let ((g-table (hulls-to-hashmap convex check-masks)))
+    (labels
+        ((process-existance (mask)
+           (maphash 
+             (lambda (key value)
+               (when
+                   (and
+                     (contains mask key)
+                     (not (contains mask value)))
+                 (return-from process-existance nil)))
+             g-table)
+           t)
+         (process-mask (mask)
+           (if (member mask convex :test #'equal)
+               (process-existance mask)
+               (not (process-existance mask)))))
+      (mapcar #'process-mask masks))))
+
 (set-of-all-subsets (get-all-masks 3))
 
-(check-ngar-display '((1 1 1) (1 1 0) (1 0 1) (0 1 1) (1 0 0)) 1 '(a b c))
-
-(let* ((sset (set-of-all-subsets (get-all-masks 3)))
+(let* ((dim 3)
+       (masks (get-all-masks dim))
+       (sset (set-of-all-subsets masks))
        (cset (remove-if-not 
               (lambda (x) (and
-                           (member '(1 1 1) x :test #'equal)
-                           (member '(0 0 0) x :test #'equal)))
-              sset)))
+                           (member (make-list dim :INITIAL-ELEMENT 1) x :test #'equal)
+                           (member (make-list dim :INITIAL-ELEMENT 0) x :test #'equal)))
+              sset))
+       (chkm (find-check-masks masks 0)))
   (loop for convex in cset do
-        (check-ngar-display convex 0 '(a b c))))
+        (check-ngar-display masks convex chkm '(a b c))))
 
-(let ((table (find-hulls '((1 1 1 1) (0 1 1 1) (0 1 0 1)) 1)))
-  (maphash 
-   (lambda (key value) (format t "g: ~a → ~a~%" key value))
-   table)
-  (hash-table-count table))
+(defparameter *alphabet*
+  '(a b c d e f g h k l m n o p r s q u w x y z))
+
+(let* ((dim 2)
+       (ngar 0)
+       (masks (get-all-masks dim))
+       (sset (set-of-all-subsets masks))
+       (cset (remove-if-not 
+              (lambda (x) (and
+                           (member (make-list dim :INITIAL-ELEMENT 1) x :test #'equal)))
+              sset))
+       (chkm (find-check-masks masks ngar)))
+  (loop for convex in cset do
+        (let* ((pre-res (check-ngar masks convex chkm))
+               (res (eval (cons 'and pre-res))))
+          (format t "~a ↦ ~a ↦ ~a~%" (print-set (mapcar (curry #'print-mask (subseq *ALPHABET* 0 dim)) convex)) pre-res res))))
+
+(let* ((dim 4)
+       (ngar 4)
+       (masks (get-all-masks dim))
+       (sset (set-of-all-subsets masks))
+       (cset (remove-if-not 
+              (lambda (x) (and
+                           (member (make-list dim :INITIAL-ELEMENT 1) x :test #'equal)))
+              sset))
+       (chkm (find-check-masks masks ngar)))
+  (reduce #'+ 
+          (loop for convex in cset collect
+               (let* ((pre-res (check-ngar masks convex chkm))
+                      (res (eval (cons 'and pre-res))))
+                 (if res 1 0)))))
 
 (get-all-masks 3)
 
 ;(remove-if-not (lambda (x) (<= (length x) 1)) (set-of-all-subsets '(a b c)))
 
+(eval (cons 'and '(t nil t)))
+(subseq '(a b c d e f) 0 3)
